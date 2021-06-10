719b40575b15ff45aec0aa356108f3be7a5a2343
Thang Le Quoc <thanglequoc.it@gmail.com>
2021-06-10 08:38:48 +0700

    Add Simple Kafka producer

---
+++ README.md
@@ -0,0 +1,11 @@
+# Brief
+This is my kafka repository to learn Udemy Kafka course
+# Known issue
+**Setup**
+Project running on IntellJ on Windows 10 machine  
+Kafka server running in WSL2 Ubuntu distribution.
+
+There was a connection problem from Windows to Kafka on WSL2. I had to change
+the listener properties of Kafka server and set on Windows accordingly.  
+See issue on https://stackoverflow.com/a/66379094/5668956
+
---
+++ src/main/java/com/github/thanglequoc/kafka/Main.java
@@ -0,0 +1,41 @@
+package com.github.thanglequoc.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Create producer properties
+        Properties properties = new Properties();
+
+        String originalBootstrapServerAddr = "127.0.0.1:9092";
+        String altWSLBootstrapServerAddr = "[::1]:9092";
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, altWSLBootstrapServerAddr);
+        // see issue on https://stackoverflow.com/a/66379094/5668956
+
+        // this serializer help the producer know what kind of data being send to kafka
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // Create producer record
+        ProducerRecord<String, String> record = new ProducerRecord<>("second_topic", "Hello world");
+
+        producer.send(record);
+
+        // flush the data
+        // producer.flush();
+        producer.close();
+
+
+        System.out.println("Hello World");
+    }
+}
